3f4c400e6596763022ad14518955fa24959af6df
Sage <sagelt@gmail.com>
2018-08-20 20:39:58 -0700

    Load files locally and stop checking for wakes

--- music.py
+++ music.py
@@ -3,7 +3,7 @@ import pygame
 pygame.mixer.init()
 
 def start():
-  pygame.mixer.music.load('/Users/sage/Documents/GitHub/goto_sleep/rain.ogg')
+  pygame.mixer.music.load('./rain.ogg')
   pygame.mixer.music.play(loops=-1)
 
 def stop():
